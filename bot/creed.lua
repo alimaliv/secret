bcf5ba9693de06da0476803bb3d81c78065bbd65
alimaliv <Mav.maliv.2002@gmail.com>
2016-02-13 20:56:28 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {100610521},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
